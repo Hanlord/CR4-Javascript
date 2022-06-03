8656c9d5e935069c85f945067ca7bc4079db7601
We-Han <chen007@live.at>
2022-06-04 00:36:13 +0200

    update

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function updateHTML(arr) {
     <p class="card-text">${x.text}</p><hr>
     <div class="d-flex justify-content-start">
     <i class="fa-solid fa-triangle-exclamation"></i>
-    <p>&ensp;Priority level: &nbsp;</p> <a class="btn-prio btn btn-success"><span class="result">${x.imp}</span></a>
+    <p>&ensp;Priority level: &nbsp;</p> <a id="button" class="btn-prio btn btn-success"><span class="result">${x.imp}</span></a>
     </div>
     <div class="d-flex justify-content-start">
     <i class="fa-solid fa-calendar-days"></i>
@@ -36,27 +36,45 @@ function updateHTML(arr) {
     `;
   }
   counterAction();
+  change();
 }
 
 function counterAction() {
   let btns = document.getElementsByClassName("btn-prio");
+  let count = 0;
   for (let i = 0; i < btns.length; i++ ) {
     btns[i].addEventListener("click", function () {
       tasks[i].imp++;
       document.getElementsByClassName("result")[i].innerHTML = tasks[i].imp;
-      if(btns[i]>2){
-        document.getElementsbyClassName("result")[i].innerHTML.style.backgroundColor="yellow";
-        return
-      } else if(btns[i]>5){
-        document.getElementsbyClassName("result")[i].innerHTML.style.backgroundColor="red";
-      }
-  
-
       var sortedArray = tasks.sort((a, b) => b.imp - a.imp);
       updateHTML(sortedArray);
     });
   }
 }
+// color change
+function change(){
+  let btns = document.getElementsByClassName("btn-prio");
+  console.log(btns);
+  switch (true){
+    case btns > 2:
+    btns.style.backgorundColor="yellow";
+    console.log(btns);
+
+    case btns > 4:
+      btns.style.backgroundColor="red";
+      console.log(btns);
+    
+  }
+}
+updateHTML(tasks);
+counterAction();
+change();
+changeColor();
+
+
+
+
+//Sort Button
 
 // function sortprio() {
 //   let btnprio = document.getElementById("sorti");
@@ -64,41 +82,32 @@ function counterAction() {
 //     btnprio[i].addEventListener("click", function(){
 //       tasks[i].sort((a, b)=> b.imp - a.imp);
 //       updateHTML(sortprio);
-//       var sortedArray = tasks.sort((a, b) => b.imp - a.imp);
-//       updateHTML(sortedArray);
+//       let sortedArray = tasks.sort((a, b) => b.imp - a.imp);
+//       console.log(sortedArray);
 //     })
 //   }
-//   var sortedArray = tasks.sort((a, b) => b.imp - a.imp);
-//   updateHTML(sortedArray);
-  
-  
 // }
+// sortprio(sortedArray);
 
-// const btnArr2 = document.getElementsbyClassName("btn-prio")
-// const btnArr = document.getElementById("sorti");
-// for(i = 0;i < btnArr.length;i++){
-//   btnArr[i].addEventlistener("click",changeColor);
-// }
-// function changeColor(){
-//   let red = Math.floor(Math.random() * 255);
-//   let green = Math.floor(Math.random() * 255);
-//   let blue = Math.floor(Math.random() * 255);
-//   let btns = document.getElementsByClassName("btn-prio")
-//   if(btns[i]>2){
-//     document.getElementsbyClassName("btn-prio")[i].innerHTML.style.backgroundColor="yellow";
-//     return
-//   } else if(btns[i]>5){
-//     document.getElementsbyClassName("btn-prio")[i].innerHTML.style.backgroundColor="red";
-//   }
-//   document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
-// }
-// changeColor()
+
+// test
+
+const btnArr = document.getElementsByTagName("btn-prio");
+        for(i = 0; i < btnArr.length; i++){
+            btnArr[i].addEventListener("click", changeColor);
+        }
+        function changeColor(){
+            let red = Math.floor(Math.random() * 255);
+            let green = Math.floor(Math.random() * 255);
+            let blue = Math.floor(Math.random() * 255);
+            document.button.style.backgroundColor = `rgb(${red},${green},${blue})`;
+        }
 
 
 
 
-updateHTML(tasks);
-counterAction();
+
+
 
 
 
